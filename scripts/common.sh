29c3e98b5969177d58c96cf3705cae290d94c0d5
Yunxing Dai <yunxing@cloudwalk.me>
2016-09-12 12:22:44 -0700

    init commit

---
+++ scripts/common.sh
@@ -0,0 +1,34 @@
+read -r -d '' greetingMsg << EOM
+================================================================
+       ___  _______   ________  _  __
+      / _ \/ __/ _ | / __/ __ \/ |/ /
+     / , _/ _// __ |_\ \/ /_/ /    /
+    /_/|_/___/_/ |_/___/\____/_/|_/
+
+================================================================
+
+reshell is a virtual environment for reason
+
+Supported commands
+
+  rtop -- reason REPL
+
+  refmt -- converting between syntaxes
+
+  rec -- compilation in bytecode mode
+
+  reopt -- compilation in native mode
+
+[for contributors only]:
+
+  reason-test -- run "make test" in node_modules/reason
+
+  reason-install -- (re)install reason from node_modules/reason
+
+  reason-dir -- print the path of reason being used currently
+
+  reason-help -- show this message
+
+================================================================
+
+EOM
